00dc795a25b13f052c1554190b0bb1abd2aaa6c4
Deekahy <Lennart.Diego.Kahn@gmail.com>
2024-08-15 14:00:32 +0200

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Rust project with required dependencies and automatic cargo run";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            pkgs.rustup
+            pkgs.nodejs
+            pkgs.cargo-tauri
+            pkgs.pkg-config
+            pkgs.cargo-tauri
+          ];
+
+          shellHook = ''
+            rustup toolchain install stable
+            rustup default stable
+            rustup target add wasm32-unknown-unknown
+            cargo install trunk
+            cargo install cargo-leptos
+            cargo leptos watch
+          '';
+        };
+      });
+}
