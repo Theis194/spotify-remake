d7386606d1947477937f5e5fb7e7af4623643557
Theis Jensen <53541469+Theis194@users.noreply.github.com>
2024-08-16 22:48:13 +0200

    ui for displaying songs, albums, and artists

--- src/pages/search.rs
+++ src/pages/search.rs
@@ -8,6 +8,13 @@ extern "C" {
     async fn invoke(cmd: &str, args: JsValue) -> JsValue;
 }
 
+struct Song {
+    name: String,
+    artist: String,
+    duration: i32,
+    image_src: String,
+}
+
 #[component]
 pub fn Search() -> impl IntoView {
     let (spotifyClientID, setSpotifyClientID) = create_signal(String::new());
@@ -25,19 +32,53 @@ pub fn Search() -> impl IntoView {
         });
     });
 
+    let songs = vec![
+        Song {
+            name: "Song 1".to_string(),
+            artist: "Artist 1".to_string(),
+            duration: 180,
+            image_src: "https://via.placeholder.com/150".to_string(),
+        },
+        Song {
+            name: "Song 2".to_string(),
+            artist: "Artist 2".to_string(),
+            duration: 240,
+            image_src: "https://via.placeholder.com/150".to_string(),
+        },
+        Song {
+            name: "Song 3".to_string(),
+            artist: "Artist 3".to_string(),
+            duration: 300,
+            image_src: "https://via.placeholder.com/150".to_string(),
+        },
+        Song {
+            name: "Song 4".to_string(),
+            artist: "Artist 5".to_string(),
+            duration: 360,
+            image_src: "https://via.placeholder.com/150".to_string(),
+        },
+    ];
+
     view! {
         <div class="flex flex-col lg:flex-row gap-4">
             <div>
-                <h2>Best Match</h2>
-                <div class="flex-none w-full lg:w-96 bg-neutral p-4 rounded">
+                <h2 class="pb-2">Best Match</h2>
+                <div class="flex-none w-96 bg-neutral p-4 rounded">
                     <img src="https://via.placeholder.com/150" alt="Spotify Logo" class="object-cover rounded-full size-24"/>
-                    <h2 id="name" class="text-4xl text-neutral-content">name</h2>
+                    <h2 id="name" class="text-4xl text-neutral-content pt-4">name</h2>
                     <h3 id="type" class="text-xl text-neutral-content px-4 bg-base-100 rounded-full w-fit">type</h3>
                 </div>
             </div>
                 
             <div>
                 <h2>Songs</h2>
+                
+                <div>
+                    {songs.into_iter()
+                        .map(|n| song_list_item(n))
+                        .collect::<Vec<_>>()}
+                </div>
+              
                 <div class="flex-1 w-full p-4">
                     <h2 class="break-all">{move || format!("Spotify Client ID: {}", spotifyClientID.get())}</h2>
                     <h2 class="break-all">{move || format!("Spotify Secret: {}", spotifySecret.get())}</h2>
@@ -45,4 +86,36 @@ pub fn Search() -> impl IntoView {
             </div>
         </div>
     }
+}
+
+fn song_list_item(song: Song) -> impl IntoView {
+    let min = song.duration / 60;
+    let sec = song.duration % 60;
+
+    view! {
+        <div class="flex justify-between items-center bg-base-100 rounded px-2 py-2 hover:bg-neutral">
+            <div class="flex items-center">
+                <img src={song.image_src} class="size-8"/>
+                <div class="px-4">
+                    <h2 class="text-base -mb-1 text-neutral-content">{song.name}</h2>
+                    <h3 class="text-sm text-neutral-content">{song.artist}</h3>
+                </div>
+            </div>
+
+            <div class="flex text-neutral-content">
+                <p>{format_time(song.duration)}</p>
+            </div>
+        </div>
+    }
+}
+
+fn format_time(time: i32) -> String {
+    let min = time / 60;
+    let sec = time % 60;
+
+    if sec < 10 {
+        return format!("{}:0{}", min, sec)
+    }
+
+    format!("{}:{}", min, sec)
 }
